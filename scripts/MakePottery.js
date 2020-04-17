19d06ed2591427683c67ffee6239c7bafea83539
durrjp <durrjp1@gmail.com>
2020-04-17 13:12:03 -0500

    return the list of sellable pots

---
+++ scripts/MakePottery.js
@@ -0,0 +1,81 @@
+
+let idCounter = 0
+let potteryObj = {}
+
+const MakePottery = (shape, weight, height) => {
+    idCounter += 1
+    potteryObj = {
+        id: idCounter,
+        shape: shape,
+        weight: weight,
+        height: height
+    }
+    return potteryObj
+}
+
+const KilnFunction = (potteryObj, temperature) => {
+    potteryObj.fired = true
+    if (temperature > 2200) {
+        potteryObj.cracked = true
+    } else {
+        potteryObj.cracked = false
+    }
+    return potteryObj
+}
+
+const potteryToSell = []
+
+const ToSellOrNotToSell = (potteryObj) => {
+    potteryObj.weight > 3 ? potteryObj.price=40 : potteryObj.price=20
+    if (potteryObj.cracked === false) {
+        potteryToSell.push(potteryObj)
+    }
+    console.log(potteryObj)
+    console.log(potteryToSell)
+
+}
+
+const potParameters = [
+    {
+        shape: "circle",
+        weight: 20,
+        height: 10,
+        temperature: 2100
+    },
+    {
+        shape: "vase shape",
+        weight: 5,
+        height: 2,
+        temperature: 1800
+    },
+    {
+        shape: "square",
+        weight: 10,
+        height: 2,
+        temperature: 2400
+    },
+    {
+        shape: "circle",
+        weight: 5,
+        height: 5,
+        temperature: 2200
+    },
+    {
+        shape: "U-shaped",
+        weight: 1,
+        height: 1,
+        temperature: 2150
+    },
+]
+
+const PotsAssembly = () => {
+    potParameters.map(potParamObject => {
+        const madePot = MakePottery(potParamObject.shape, potParamObject.weight, potParamObject.height)
+        const firedPot = KilnFunction(madePot, potParamObject.temperature)
+        ToSellOrNotToSell(firedPot)
+    })
+    console.log(potteryToSell)
+}
+
+PotsAssembly()
+
